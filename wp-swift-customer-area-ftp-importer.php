0486eabcfa1619880a780ef061ccd8e10c40701a
Gary Swift <garyswiftmail@gmail.com>
2018-03-04 15:35:36 +0000

    first commit

--- wp-swift-customer-area-ftp-importer.php
+++ wp-swift-customer-area-ftp-importer.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: WP Swift: Customer Area FTP Importer
-Plugin URI: 
+Plugin URI: https://github.com/wp-swift-wordpress/wp-swift-customer-area-ftp-importer
 Description: Parses the WP Customer Area assigned FTP folder and facilitates the importing of PDFs into WP Customer Area private files.
 Version: 1
 Author: Gary Swift
